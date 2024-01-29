85b0a12f7ac69acd51ad89f4b6f399aba3bb1682
ThanhTan <thanhtan27032001@gmail.com>
2024-01-29 22:37:50 +0700

    [v1.0.0] Home - layout

---
+++ app/src/main/java/com/example/musicplayer/controller/adapter/SongsChartAdapter.kt
@@ -0,0 +1,23 @@
+package com.example.musicplayer.controller.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class SongsChartAdapter: RecyclerView.Adapter<SongsChartAdapter.MyViewHolder>() {
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
