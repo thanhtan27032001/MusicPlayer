19cc4c62c4eb6a661aa0689d7fc202e14a3f056b
ThanhTan <thanhtan27032001@gmail.com>
2024-01-29 23:19:13 +0700

    [v1.0.0] Home - UI item song chart

--- app/src/main/java/com/example/musicplayer/controller/adapter/SongsChartAdapter.kt
+++ app/src/main/java/com/example/musicplayer/controller/adapter/SongsChartAdapter.kt
@@ -1,20 +1,35 @@
 package com.example.musicplayer.controller.adapter
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.musicplayer.R
+import com.example.musicplayer.controller.MainActivity
+import com.example.musicplayer.model.SongFake
 
-class SongsChartAdapter: RecyclerView.Adapter<SongsChartAdapter.MyViewHolder>() {
+class SongsChartAdapter : RecyclerView.Adapter<SongsChartAdapter.MyViewHolder> {
     class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     }
 
+    val songsChart: Array<SongFake>
+    val context: MainActivity
+
+    constructor(songsChart: Array<SongFake>, context: MainActivity) : super() {
+        this.songsChart = songsChart
+        this.context = context
+    }
+
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        TODO("Not yet implemented")
+        return MyViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_song_chart, parent, false)
+        )
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return songsChart.size
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
---
+++ app/src/main/java/com/example/musicplayer/model/SongFake.kt
@@ -0,0 +1,13 @@
+package com.example.musicplayer.model
+
+class SongFake {
+    var name: String
+    var performers: String
+    var imageUrl: String
+
+    constructor(name: String, performers: String, imageUrl: String) {
+        this.name = name
+        this.performers = performers
+        this.imageUrl = imageUrl
+    }
+}
